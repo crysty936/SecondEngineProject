ca36f9e56ff28f5adffbdd083c9237c55357a3a9
Bobescu Cristian <crysty936@gmail.com>
2021-11-03 00:22:41 +0200

    Converted project to use CMake Implemented main loop method

--- Engine/Source/Core/Private/EngineCore.cpp
+++ Engine/Source/Core/Private/EngineCore.cpp
@@ -1,31 +1,55 @@
-#include "Core/Public/EngineCore.h"
 #include <assert.h>
+#include <thread>
+#include <iostream>
+#include <windows.h>
+#include "Core/Public/EngineCore.h"
+#include "GLFW/glfw3.h"
+
+const float IdealFrameRate = 120.f;
 
 EngineCore Engine;
 
 EngineCore::EngineCore()
 {
-	static bool engineExisting = false;
-	assert(!engineExisting);
+	static bool engineExists = false;
+	assert(!engineExists);
 
-	engineExisting = true;
+	engineExists = true;
 }
 
 EngineCore::~EngineCore() = default;
 
 void EngineCore::Init()
 {
+	glfwInit();
+
 
 }
 
 void EngineCore::Run()
 {
-	
-	
+	double deltaTime = 0.0;
+	double lastTime = glfwGetTime();
+	const float idealFrameTime = 1.0f / IdealFrameRate;
 
-
-
-
-
+ 	while (true)
+ 	{
+ 		double currentTime = glfwGetTime();
+ 		double timeSpent = (currentTime - lastTime);
+ 		// sleep for x amount to reach the ideal frame time
+ 		double sleepTime = idealFrameTime - timeSpent;
+ 		if (sleepTime)
+ 		{
+            uint32_t milliseconds = (uint32_t)(sleepTime * 1000.0);
+            ::Sleep(milliseconds);
+ 		}
+ 
+ 		currentTime = glfwGetTime();
+ 		deltaTime = currentTime - lastTime;
+ 		lastTime = currentTime;
+ 
+ 		std::cout << "Frame time: " << deltaTime << std::endl;
+ 			
+ 	}
 }
 
