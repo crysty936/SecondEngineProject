fe99456436003e20757af34b5ad212896ddf0c19
Bobescu Cristian <crysty936@gmail.com>
2022-02-26 00:41:50 +0200

    git ignore /build and /EngineBuild

--- TestProjects/3DTest/EngineBuild/Plugins/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/SecondEngineProject/Plugins/assimp/contrib/zlib
-# Build directory: D:/Projects/SecondEngineProject/TestProjects/3DTest/EngineBuild/Plugins/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- TestProjects/3DTest/EngineBuild/Plugins/googletest-main/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/SecondEngineProject/Plugins/googletest-main/googletest
-# Build directory: D:/Projects/SecondEngineProject/TestProjects/3DTest/EngineBuild/Plugins/googletest-main/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
